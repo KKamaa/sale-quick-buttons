1f23e120798df6ccf26379face8a0a6c77466e90
KKamaa <kevkamaa96@gmail.com>
2019-06-21 05:28:49 +0300

    [ADD] sale_quotations_btns v11

--- sale_quotation_btns/__manifest__.py
+++ sale_quotation_btns/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 #   © 2019 Kevin Kamau
 #   License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Sale Quotation Buttons",
-    'version': '10.0.1.0',
+    'version': '11.0.1.0',
     'depends': ['sale'],
     'author': "Kevin Kamau",
     'category': 'Sales',
